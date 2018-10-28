5a95157da717d810baaad340a0a94b11bf6636b5
z_goodman <zgoodman@live.unc.edu>
2018-10-27 23:25:40 -0400

    first commit, testing button creation and decoration

--- generalBuild.py
+++ generalBuild.py
@@ -1,12 +1,11 @@
 import tkinter as tk
-from tkinter import *
 
 root = tk.Tk()
 w = root.winfo_screenwidth()
 h = root.winfo_screenheight()
 root.geometry("%dx%d" % (w, h))
 
-button = Button(root, text="hahaha")
+button = tk.Button(root, text="Hello, world", fg='red', bg='black')
 button.pack()
 
 root.mainloop()
