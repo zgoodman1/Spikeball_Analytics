d5b82dac63c0ea02395bf76da07ccbc9a4b22977
z_goodman <zgoodman@live.unc.edu>
2019-01-26 16:19:44 -0500

    revised serve/return order

--- main_app/main.py
+++ main_app/main.py
@@ -5,38 +5,58 @@ team2 = Team()
 
 game_to = input("Game to? ")
 
-server1 = input("First server? ")
-server2 = input("Second server? ")
 serve_order = []
+return_order = []
 
-if server1 == team1.player1.name and server2 == team2.player1.name:
+player1 = team1.player1.name
+player2 = team1.player2.name
+player3 = team2.player1.name
+player4 = team2.player2.name
 
-    serve_order = [team1.player1.name, team2.player1.name, team1.player2.name, team2.player2.name]
+server1 = input("First server? ")
 
-elif server1 == team1.player1.name and server2 == team2.player2.name:
+if server1 == player1:
+    serve_order = [player1, player3, player2, player4]
+    returning = input("Who is returning? ")
+    if returning == player3:
+        return_order = [player3, player1, player4, player2]
+    else:
+        return_order = [player4, player2, player3, player1]
 
-    serve_order = [team1.player1.name, team2.player2.name, team1.player2.name, team2.player1.name]
+elif server1 == player2:
+    serve_order = [player2, player3, player1, player4]
+    returning = input("Who is returning? ")
+    if returning == player3:
+        return_order = [player3, player1, player4, player2]
+    else:
+        return_order = [player4, player2, player3, player1]
 
-elif server1 == team1.player2.name and server2 == team2.player1.name:
-
-    serve_order = [team1.player2.name, team2.player1.name, team1.player1.name, team2.player2.name]
-
-elif server1 == team1.player2.name and server2 == team2.player2.name:
-
-    serve_order = [team1.player2.name, team2.player2.name, team1.player1.name, team2.player1.name]
-#######################################
-elif server1 == team2.player1.name and server2 == team1.player1.name:
-
-    serve_order = [team2.player1.name, team1.player1.name, team2.player2.name, team1.player2.name]
-
-elif server1 == team2.player1.name and server2 == team1.player2.name:
-
-    serve_order = [team2.player1.name, team1.player2.name, team2.player2.name, team1.player1.name]
-
-elif server1 == team2.player2.name and server2 == team1.player1.name:
-
-    serve_order = [team2.player2.name, team1.player1.name, team2.player1.name, team1.player2.name]
+elif server1 == player3:
+    serve_order = [player3, player1, player4, player2]
+    returning = input("Who is returning? ")
+    if returning == player1:
+        return_order = [player1, player3, player2, player4]
+    else:
+        return_order = [player2, player4, player1, player3]
 
 else:
+    serve_order = [player4, player1, player3, player2]
+    returning = input("Who is returning? ")
+    if returning == player2:
+        return_order = [player2, player3, player1, player4]
+    else:
+        return_order = [player1, player4, player2, player3]
 
-    serve_order = [team2.player2.name, team1.player2.name, team2.player1.name, team1.player1.name]
+
+
+
+
+
+
+
+
+
+
+
+
+
