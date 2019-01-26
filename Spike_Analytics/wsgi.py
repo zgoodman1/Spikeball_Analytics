fc588260b661f94137c0c601c7cb984bdd478a6d
z_goodman <zgoodman@live.unc.edu>
2019-01-26 17:21:22 -0500

    began implementing django framework

--- Spike_Analytics/wsgi.py
+++ Spike_Analytics/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Spike_Analytics project.
+WSGI config for spike_analytics project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Spike_Analytics.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'spike_analytics.settings')
 
 application = get_wsgi_application()
