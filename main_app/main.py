9c0045f23690b36c1b986f6d02a8203fec137b86
z_goodman <zgoodman@live.unc.edu>
2019-01-26 15:21:48 -0500

    made main file

--- main_app/main.py
+++ main_app/main.py
@@ -40,5 +40,3 @@ elif server1 == team2.player2.name and server2 == team1.player1.name:
 else:
 
     serve_order = [team2.player2.name, team1.player2.name, team2.player1.name, team1.player1.name]
-
-print(serve_order)
